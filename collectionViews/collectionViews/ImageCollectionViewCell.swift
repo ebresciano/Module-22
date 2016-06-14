fc90c0e5d7c42c13842dd175484dae4f4fba4d29
Eva <Bresciano>
2016-06-14 13:53:45 -0600

    guided project finished

---
+++ collectionViews/collectionViews/ImageCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCollectionViewCell.swift
+//  collectionViews
+//
+//  Created by Eva Marie Bresciano on 6/14/16.
+//  Copyright © 2016 Eva Bresciano. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+}
